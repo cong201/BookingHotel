f29e33a9b511423193e127d0beba4de6317ebe67
Công Lê <levancong_t64@gmail.com>
2023-07-21 00:12:08 +0700

    create app and connect to mongosedb

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoute from './routes/auth.js'
+import userRoute from './routes/user.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
+
+const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoose");
+    } catch (error) {
+        throw error
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoose disconnected");
+})
+
+//middleware
+app.use("/api/auth", authRoute)
+app.use("/api/user", userRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/rooms", roomsRoute)
+
+
+app.listen(3300, () => {
+    connect()
+    console.log("Connect to backend server");
+})
---
+++ routes/hotels.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+//Create
+router.post('/', (req, res) => {
+
+})
+
+export default router
