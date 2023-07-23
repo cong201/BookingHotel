268ec7294162dd7dfaa4a6ba46f36c3e2bcf6495
Công Lê <levancong_t64@gmail.com>
2023-07-23 12:47:26 +0700

    update phan post user(authentication)

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,8 @@
 import express from 'express';
+import { register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("hello")
-})
-
-router.get('/register', (req, res) => {
-    res.send("hihi")
-})
+router.post('/register', register)
 
 export default router
